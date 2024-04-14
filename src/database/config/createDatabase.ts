f6df3ef907c3b377ff0f4b340f3e95b383e9cb27
Hardik Chauhan <hardik.chauhan@opensesame.com>
2024-04-14 16:35:51 +0530

    Initial commit - server

--- src/database/config/createDatabase.ts
+++ src/database/config/createDatabase.ts
@@ -1,5 +1,5 @@
 import logger from '../../utils/logger'
-import config from '../config/config'
+import config from './config'
 
 const env: string = process.env.NODE_ENV || 'development'
 const dbConfig = config[env]
